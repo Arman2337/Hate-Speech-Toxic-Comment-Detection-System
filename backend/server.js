73fb1c607e526e54f6314927a5b13a65289b6efd
Arman2337 <23it119@charusat.edu.in>
2025-08-15 21:22:56 +0530

    update server add cokkies session

--- backend/server.js
+++ backend/server.js
@@ -5,6 +5,7 @@ const connectDB = require("./config/db");
 
 const authRoutes = require("./routes/authRoutes");
 const predictRoutes = require("./routes/predictRoutes");
+const cookieParser = require("cookie-parser");
 
 const app = express();
 const PORT = process.env.PORT || 5000;
@@ -15,6 +16,7 @@ app.use(cors({
   credentials: true
 }));
 app.use(express.json());
+app.use(cookieParser()); 
 
 // Connect to MongoDB
 connectDB();
