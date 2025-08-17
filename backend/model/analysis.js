f25b01b326b6bd9be98c3074d592ea2d530bcf9e
Arman2337 <23it119@charusat.edu.in>
2025-08-17 16:37:31 +0530

    update analysis

--- backend/model/analysis.js
+++ backend/model/analysis.js
@@ -1,9 +1,19 @@
 const mongoose = require("mongoose");
 
 const AnalysisSchema = new mongoose.Schema({
-  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" },
+  userId: { type: mongoose.Schema.Types.ObjectId, ref: "User" , required:false },
   inputText: { type: String, required: true },
-  results: { type: Object, required: true },
+  results: {
+    overallScore: Number,
+    categories: {
+      toxic: Number,
+      severe_toxic: Number,
+      obscene: Number,
+      threat: Number,
+      insult: Number,
+      identity_hate: Number
+    }
+  },
   createdAt: { type: Date, default: Date.now },
 });
 
