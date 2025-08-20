fa3ba31fe36836972209f6459d402df764e76c1e
Arman2337 <23it119@charusat.edu.in>
2025-08-20 22:44:07 +0530

    update predict

--- backend/ml/predict.py
+++ backend/ml/predict.py
@@ -1,30 +1,3 @@
-# import sys
-# import json
-# import joblib
-# from preprocess import clean_text
-
-# # Load model & vectorizer
-# model = joblib.load("model.pkl")
-# vectorizer = joblib.load("vectorizer.pkl")
-# labels = ["toxic", "severe_toxic", "obscene", "threat", "insult", "identity_hate"]
-
-# def predict(text):
-#     cleaned = clean_text(text)
-#     vect = vectorizer.transform([cleaned])
-#     result = model.predict(vect)
-#     matched = [labels[i] for i, val in enumerate(result[0]) if val == 1]
-#     return {
-#         "toxic": bool(matched),
-#         "labels": matched
-#     }
-
-# # CLI support: test from terminal
-# if __name__ == "__main__":
-#     input_text = sys.argv[1]
-#     output = predict(input_text)
-#     print(json.dumps(output))
-
-
 import os
 import sys
 import json
